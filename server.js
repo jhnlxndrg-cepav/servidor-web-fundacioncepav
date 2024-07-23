ab57f754becd0c2a482d596bac905d8228339c37
oscargb <alexvillalobos313@gmail.com>
2024-07-22 22:24:02 -0500

    Actividad realizada

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/estudiantes', function (request, response) {
     response.send('Hola Mundo!')
 })
 
+app.post('/estados', function (request, response) {
+    response.send('Tarea Finalizada')
+})
 // starts a simple http server locally on port 3000
 // server.listen(3000, '127.0.0.1', () => {
 //     console.log('Listening on 127.0.0.1:3000');
